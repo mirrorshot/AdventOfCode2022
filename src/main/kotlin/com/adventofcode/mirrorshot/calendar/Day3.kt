778faab6d9c125cee49f98b4a702697c8d833f22
BrambillaDa <daniele.brambilla@nttdata.com>
2022-12-03 14:07:55 +0100

    Day3 solutions

---
+++ src/main/kotlin/com/adventofcode/mirrorshot/calendar/Day3.kt
@@ -0,0 +1,41 @@
+package com.adventofcode.mirrorshot.calendar
+
+class Day3 {
+    fun problem1(input: ByteArray): Long {
+        return extractMatches(input)
+            .map { e -> Pair(e.substring(0, e.length / 2), e.substring(e.length / 2)) }
+            .map { e -> Pair(e, score(e)) }
+            .also { m -> println(m) }
+            .sumOf { m -> m.second.second }
+    }
+
+    fun problem2(input: ByteArray): Long {
+        return extractMatches(input)
+            .chunked(3)
+            .map { g -> Pair(g, score(g)) }
+            .also { m -> println(m) }
+            .sumOf { m -> m.second.second }
+    }
+
+    private fun extractMatches(input: ByteArray) = input.decodeToString().split("\n")
+        .filter { e -> e.isNotBlank() }
+
+    private fun score(rucksack: Pair<String, String>): Pair<Char, Long> =
+        rucksack.first
+            .filter { c -> rucksack.second.contains(c) }
+            .map { r -> Pair(r, priority(r)) }
+            .first()
+
+    private fun score(rucksacks: List<String>): Pair<Char, Long> =
+        rucksacks[0]
+            .filter { i -> rucksacks[1].contains(i) && rucksacks[2].contains(i) }
+            .map { c -> Pair(c, priority(c)) }
+            .first()
+
+    private fun priority(item: Char): Long = when (item) {
+        in 'a'..'z' -> item - 'a' + 1L
+        in 'A'..'Z' -> item - 'A' + 27L
+        else -> 0L
+    }
+
+}
--- src/test/kotlin/com/adventofcode/mirrorshot/calendar/Day3Test.kt
+++ src/test/kotlin/com/adventofcode/mirrorshot/calendar/Day3Test.kt
@@ -1,41 +1,47 @@
 package com.adventofcode.mirrorshot.calendar
 
+import org.junit.Ignore
 import org.junit.jupiter.api.Test
 import java.io.File
 import kotlin.test.assertEquals
 
-class Day2Test {
-    private val input = """A Y
-B X
-C Z"""
-    private val inputFile = File(ClassLoader.getSystemResource("day2.txt").file)
-    private val solver = Day2()
+class Day3Test {
+    private val input = """vJrwpWtwJgWrhcsFMMfFFhFp
+jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+PmmdzqPrVvPwwTWBwg
+wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+ttgJtRGJQctTZtZT
+CrZsJsPPZsGzwwsLwLmpwMDw"""
+    private val inputFile = File(ClassLoader.getSystemResource("day3.txt").file)
+    private val solver = Day3()
 
     @Test
     fun problem1() {
         val byteArrayInput = input.toByteArray()
         val result = solver.problem1(byteArrayInput)
-        assertEquals(15, result)
+        assertEquals(157, result)
     }
 
+    @Ignore
     @Test
     fun problem2() {
         val byteArrayInput = input.toByteArray()
         val result = solver.problem2(byteArrayInput)
-        assertEquals(12, result)
+        assertEquals(70, result)
     }
 
     @Test
     fun problem1Sol() {
         val result = solver.problem1(inputFile.readBytes())
         println(result)
-        assertEquals(14827, result)
+        assertEquals(7746, result)
     }
 
+    @Ignore
     @Test
     fun problem2Sol() {
         val result = solver.problem2(inputFile.readBytes())
         println(result)
-        assertEquals(13889, result)
+        assertEquals(2604, result)
     }
 }
