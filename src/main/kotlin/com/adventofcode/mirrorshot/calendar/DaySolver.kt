870d5735409e5f6a261bfb02dea459b89a344248
BrambillaDa <daniele.brambilla@nttdata.com>
2022-12-19 20:21:04 +0100

    Input line-separator cleanup

--- src/main/kotlin/com/adventofcode/mirrorshot/calendar/DaySolver.kt
+++ src/main/kotlin/com/adventofcode/mirrorshot/calendar/DaySolver.kt
@@ -3,6 +3,10 @@ package com.adventofcode.mirrorshot.calendar
 abstract class DaySolver<X, Y> {
 
     fun parseInput(input: ByteArray) = input.decodeToString()
+        .replace("\n\r", "\n")
+        .replace("\r\n", "\n")
+
     abstract fun problem1(input: ByteArray): X
+
     abstract fun problem2(input: ByteArray): Y
 }
